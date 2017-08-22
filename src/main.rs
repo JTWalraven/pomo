829803fb9ef6b87a95e3ae81291e734664e05d91
Jeffrey Walraven <jeffrey@ravenswall.net>
2017-08-22 19:36:45 -0400

    Added basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    // Load options using clap cli generator (yaml)
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    // Set matches to values
+    let time = value_t!(matches, "time", u32).unwrap_or(25);
+    let break_time = value_t!(matches, "break", u32).unwrap_or(5);
+    let loop_number = value_t!(matches, "loop", u32).unwrap_or(1);
+    let is_loop = matches.is_present("loop");
+    let verbose = matches.is_present("verbose");
 }
